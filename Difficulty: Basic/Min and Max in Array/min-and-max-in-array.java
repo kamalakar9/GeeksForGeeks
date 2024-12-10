283b9860b00b8c78397a59f76d6929bae87337b3
kamalakar9 <kamalakar934720@gmail.com>
2024-12-10 10:38:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
@@ -35,20 +35,18 @@ For Example -> return new Pair(minimum,maximum)
 
 class Solution {
     public Pair<Long, Long> getMinMax(int[] arr) {
-        if(arr == null || arr.length == 0){
-            return new Pair(0,0);
-        }
-        int maxVal = arr[0];
-        int minVal = arr[0];
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] > maxVal){
-                maxVal = arr[i];
-            }
-            else if(arr[i] < minVal){
-                minVal = arr[i];
-            }
-        }
-        return new Pair(minVal, maxVal);
+        // Code Here
+      int max = arr[0];
+      int min = arr[0];
+      for(int i =0; i<arr.length; i++){
+          if(arr[i] < min){
+              min = arr[i];
+          }
+          else if(arr[i] > max){
+              max = arr[i];
+          }
+      }
+      return new Pair(min, max);
     }
 }
 
