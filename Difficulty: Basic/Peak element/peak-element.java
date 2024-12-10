e5d13c51eb7863dd34878ead9617113ae631468d
kamalakar9 <kamalakar934720@gmail.com>
2024-12-10 10:09:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Peak element/peak-element.java
+++ Difficulty: Basic/Peak element/peak-element.java
@@ -19,18 +19,19 @@ class GFG {
             }
             Solution obj = new Solution();
             int f = 0;
-            int A = obj.peakElement(a);
+            int idx = obj.peakElement(a);
             int n = a.length;
-            if (A < 0 && A >= n)
+            if (idx < 0 && idx >= n)
                 System.out.println("false");
             else {
-                if (n == 1 && A == 0)
+                if (n == 1 && idx == 0)
                     f = 1;
-                else if (A == 0 && a[0] >= a[1])
+                else if (idx == 0 && a[0] > a[1])
                     f = 1;
-                else if (A == n - 1 && a[n - 1] >= a[n - 2])
+                else if (idx == n - 1 && a[n - 1] > a[n - 2])
                     f = 1;
-                else if (A >= 0 && A < n && a[A] >= a[A + 1] && a[A] >= a[A - 1])
+                else if (idx > 0 && idx < n && a[idx] > a[idx + 1] &&
+                         a[idx] > a[idx - 1])
                     f = 1;
                 else
                     f = 0;
@@ -50,18 +51,17 @@ class GFG {
 class Solution {
 
     public int peakElement(int[] arr) {
-        
+        // code here
         int n = arr.length;
+        
         if(n == 1 || arr[0] >= arr[1]){
             return 0;
         }
-        
         if(arr[n-1] >= arr[n-2]){
             return n-1;
         }
-        
-        for(int i =1; i < n-1; i++){
-            if(arr[i] >= arr[i-1] && arr[i] > arr[i+1]){
+        for(int i = 1; i < n; i++){
+            if(arr[i] >= arr[i-1] && arr[i] >= arr[i+1]){
                 return i;
             }
         }
