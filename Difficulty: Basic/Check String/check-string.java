d31c8920977d48e6a3cd4ad2fa692d39da9ad563
kamalakar9 <kamalakar934720@gmail.com>
2024-12-13 12:53:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check String/check-string.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String s = sc.next ();
+            if (new Sol().check (s))
+                System.out.println ("YES");
+            else
+                System.out.println ("NO");
+        
+System.out.println("~");
+}
+        
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    Boolean check (String s)
+    {
+        Set<Character> set = new HashSet<>();
+        for(char c : s.toCharArray()){
+            set.add(c);
+        }
+        
+        return set.size() == 1;
+    }
+}
