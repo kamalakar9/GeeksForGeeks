eee40cf478ee9231ac5481958efe505a81a77640
kamalakar9 <kamalakar934720@gmail.com>
2024-12-13 06:54:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for Binary String/check-for-binary-string.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class checkBinary {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+        while (T > 0) {
+            String str = sc.nextLine();
+            Solution g = new Solution();
+            boolean b = g.isBinary(str);
+            if (b == true)
+                System.out.println("true");
+            else
+                System.out.println("false");
+            T--;
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    boolean isBinary(String s) {
+        if(s == null || s.isEmpty()){
+            return false;
+        }
+       for(char c : s.toCharArray()){
+           if(c != '0' && c != '1'){
+               return false;
+           }
+       }
+       return true;
+    }
+}
