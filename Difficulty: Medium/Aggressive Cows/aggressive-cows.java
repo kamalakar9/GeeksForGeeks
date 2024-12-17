bfec69b30ae2571c3730da943d7be897bf975ad1
kamalakar9 <kamalakar934720@gmail.com>
2024-12-17 07:47:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.aggressiveCows(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static int aggressiveCows(int[] stalls, int k) {
+        // code here
+        
+        Arrays.sort(stalls);
+        int low = 0;
+        int high = stalls[stalls.length-1] - stalls[0];
+        int result = 0;
+        
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            if(isPossible(stalls, k, mid)){
+                result = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        
+        return result;
+    }
+    
+    static boolean isPossible(int[] stalls, int k, int minDist) {
+        
+        int count = 1;
+        int lastPosition = stalls[0];
+        for(int i = 1; i< stalls.length; i++){
+            if(stalls[i] - lastPosition >= minDist){
+                count++;
+                lastPosition = stalls[i];
+                if(count == k){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
